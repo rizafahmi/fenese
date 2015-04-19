3ae15bf0c42972f69423fdf492fc1489c0d9f839
Riza Fahmi <rizafahmi@gmail.com>
2015-04-19 21:56:30 +0700

    Remove utils.py. We didn't really need it

--- utils.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.bcrypt import generate_password_hash, check_password_hash
-
-def set_password(User, password):
-  User.password = generate_password_hash(password)
-
-  return User
-
-def validate_password(User, password):
-  return check_password_hash(User.password, password)
